4461d1ddf5c8a1c6a7bf49283158577fcc71ce79
Gray Hildreth and Nhat Hoang <grayhildreth@gmail.com>
2016-10-17 16:39:32 -0700

    add content to app component file.

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,8 +4,24 @@ import { Component } from '@angular/core';
 Component({
     selector: 'my-app',
     template: `
-    <div>Skeleton template</div>
+    <div class="container">
+      <div class="row">
+      <div class="col-sm-8"><img src="./../resources/image/Gear_Up.png" alt="" /></div>
+      <div class="col-sm-4">
+        <h4>
+          " 14"
+          <sup>degree plato</sup>
+          " |65 "
+          <sup>ibu</sup>
+          " | "
+          <sup>ALC</sup>
+          " 6.2%"
+          <sup>By Volume</sup>
+        </h4>
+      </div>
 
+      </div>
+    </div>
   `
 })
 export class AppComponent {}
